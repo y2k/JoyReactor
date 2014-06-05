ca7197aeaa17ab383aac0428a7a669a47be59a46
y2k <itwisterlx@gmail.com>
2014-06-06 02:16:47 +0400

    WP8: список постов

---
+++ JoyReactor.WP/ViewModel/PostViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoyReactor.WP.ViewModel
+{
+    public class PostViewModel : ViewModelBase
+    {
+        public ObservableCollection<ItemPostViewModel> Posts { get; private set; }
+
+        public PostViewModel()
+        {
+            Posts = new ObservableCollection<ItemPostViewModel>();
+            //
+        }
+
+        public class ItemPostViewModel : ViewModelBase
+        {
+
+        }
+    }
+}
