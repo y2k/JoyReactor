d0a4cc33a17a8997fc072fa586e2d9f158557552
y2k <itwisterlx@gmail.com>
2014-12-15 06:28:08 -0800

    cote: тесты

---
+++ JoyReactor.Core.Tests/ViewModels/FeedViewModelTests.cs
@@ -0,0 +1,88 @@
+﻿using JoyReactor.Core.Model.Inject;
+using JoyReactor.Core.Tests.Inner;
+using JoyReactor.Core.ViewModels;
+using Microsoft.Practices.ServiceLocation;
+using NUnit.Framework;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoyReactor.Core.Tests.ViewModels
+{
+    [TestFixture]
+    class FeedViewModelTests
+    {
+        const int DefaultDelay = 300;
+        FeedViewModel vm;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var provider = new DefaultServiceLocator(new TestModule());
+            ServiceLocator.SetLocatorProvider(() => provider);
+            vm = new FeedViewModel();
+        }
+
+        [Test]
+        public async Task Test()
+        {
+            await LoadFirstPage();
+            await LoadNextPage(11);
+            //await LoadNextPage(21);
+        }
+
+        async Task LoadFirstPage()
+        {
+            Assert.IsFalse(vm.IsBusy);
+            Assert.AreEqual(0, vm.Posts.Count);
+
+            vm.ChangeCurrentListIdCommand.Execute(ID.Factory.New(ID.IdConst.ReactorGood));
+
+            Assert.IsTrue(vm.IsBusy);
+            Assert.AreEqual(0, vm.Posts.Count);
+
+            await Task.Delay(DefaultDelay);
+
+            Assert.IsFalse(vm.IsBusy);
+            Assert.AreEqual(11, vm.Posts.Count);
+            CollectionAssert.AreEqual(GenerateList(10), vm.Posts, new TypeComparer());
+        }
+
+        async Task LoadNextPage(int initCount)
+        {
+            Assert.IsFalse(vm.IsBusy);
+            Assert.AreEqual(initCount, vm.Posts.Count);
+
+            GetDivider().LoadMoreCommand.Execute(null);
+
+            Assert.IsTrue(vm.IsBusy);
+            Assert.AreEqual(initCount, vm.Posts.Count);
+
+            await Task.Delay(DefaultDelay);
+
+            Assert.IsFalse(vm.IsBusy);
+            Assert.AreEqual(initCount + 10, vm.Posts.Count);
+            CollectionAssert.AreEqual(GenerateList(initCount + 10 - 1), vm.Posts, new TypeComparer());
+        }
+
+        private FeedViewModel.DividerViewModel GetDivider()
+        {
+            return vm.Posts.OfType<FeedViewModel.DividerViewModel>().First();
+        }
+
+        private static IEnumerable GenerateList(int count)
+        {
+            for (int i = 0; i < count; i++)
+                yield return new FeedViewModel.ContentViewModel(null);
+            yield return new FeedViewModel.DividerViewModel(() => { });
+        }
+
+        public class TypeComparer : IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                return x.GetType() == y.GetType() ? 0 : 1;
+            }
+        }
+    }
+}
--- JoyReactor.Core/ViewModels/FeedViewModel.cs
+++ JoyReactor.Core/ViewModels/FeedViewModel.cs
@@ -10,170 +10,175 @@ using System.Threading.Tasks;
 
 namespace JoyReactor.Core.ViewModels
 {
-	public class FeedViewModel : ViewModelBase
-	{
-		#region Commands
+    public class FeedViewModel : ViewModelBase
+    {
+        #region Commands
 
-		public RelayCommand RefreshCommand { get; set; }
+        public RelayCommand RefreshCommand { get; set; }
 
-		public RelayCommand MoreCommand { get; set; }
+        public RelayCommand MoreCommand { get; set; }
 
-		public RelayCommand ApplyCommand { get; set; }
+        public RelayCommand ApplyCommand { get; set; }
 
-		public RelayCommand<ID> ChangeCurrentListIdCommand { get; set; }
+        public RelayCommand<ID> ChangeCurrentListIdCommand { get; set; }
 
-		#endregion
+        #endregion
 
-		#region Properties
+        #region Properties
 
-		public ObservableCollection<ViewModelBase> Posts { get; } = new ObservableCollection<ViewModelBase>();
+        public ObservableCollection<ViewModelBase> Posts { get; } = new ObservableCollection<ViewModelBase>();
 
-		bool _isBusy;
+        bool _isBusy;
 
-		public bool IsBusy {
-			get { return _isBusy; }
-			set { Set (ref _isBusy, value); }
-		}
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set { Set(ref _isBusy, value); }
+        }
 
-		bool _hasNewItems;
+        bool _hasNewItems;
 
-		public bool HasNewItems {
-			get { return _hasNewItems; }
-			set { Set (ref _hasNewItems, value); }
-		}
+        public bool HasNewItems
+        {
+            get { return _hasNewItems; }
+            set { Set(ref _hasNewItems, value); }
+        }
 
-		#endregion
+        #endregion
 
-		PostCollectionModel model = new PostCollectionModel ();
-		ID id;
+        PostCollectionModel model = new PostCollectionModel();
+        ID id;
 
-		public FeedViewModel () : this (ID.Factory.New (ID.IdConst.ReactorGood))
-		{
-		}
+        public FeedViewModel() : this(ID.Factory.New(ID.IdConst.ReactorGood))
+        {
+        }
 
-		public FeedViewModel (ID id)
-		{
-			if (!IsInDesignMode) {
-				RefreshCommand = new RelayCommand (OnRefreshInvoked);
-				MoreCommand = new RelayCommand (OnButtonMoreClicked);
-				ApplyCommand = new RelayCommand (OnApplyButtonClicked);
-				ChangeCurrentListIdCommand = new RelayCommand<ID> (OnChangeCurrentListId);
-				LoadFirstPage (id);
+        public FeedViewModel(ID id)
+        {
+            RefreshCommand = new RelayCommand(OnRefreshInvoked);
+            MoreCommand = new RelayCommand(OnButtonMoreClicked);
+            ApplyCommand = new RelayCommand(OnApplyButtonClicked);
+            ChangeCurrentListIdCommand = new RelayCommand<ID>(OnChangeCurrentListId);
 
-				MessengerInstance.Register<SelectTagMessage> (this, s => OnChangeCurrentListId (s.Id));
-			}
-		}
+            MessengerInstance.Register<SelectTagMessage>(this, s => OnChangeCurrentListId(s.Id));
+        }
 
-		async void LoadFirstPage (ID newId)
-		{
-			id = newId;
+        async void LoadFirstPage(ID newId)
+        {
+            id = newId;
 
-			IsBusy = true;
-			await ReloadDataFromDatabase ();
-			await model.SyncFirstPage (id);
-			await ReloadDataFromDatabase ();
-			IsBusy = false;
-		}
+            IsBusy = true;
+            await ReloadDataFromDatabase();
+            await model.SyncFirstPage(id);
+            await ReloadDataFromDatabase();
+            IsBusy = false;
+        }
 
-		async void OnRefreshInvoked ()
-		{
-			IsBusy = true;
-			if (HasNewItems) {
-				await model.ApplyNewItems (id);
-			} else {
-				await model.Reset (id);
-				await model.SyncFirstPage (id);
-			}
-			await ReloadDataFromDatabase ();
-			IsBusy = false;
-		}
+        async void OnRefreshInvoked()
+        {
+            IsBusy = true;
+            if (HasNewItems)
+            {
+                await model.ApplyNewItems(id);
+            }
+            else
+            {
+                await model.Reset(id);
+                await model.SyncFirstPage(id);
+            }
+            await ReloadDataFromDatabase();
+            IsBusy = false;
+        }
 
-		async void OnApplyButtonClicked ()
-		{
-			await model.ApplyNewItems (id);
-			await ReloadDataFromDatabase ();
-		}
+        async void OnApplyButtonClicked()
+        {
+            await model.ApplyNewItems(id);
+            await ReloadDataFromDatabase();
+        }
 
-		async void OnButtonMoreClicked ()
-		{
-			await model.SyncNextPage (id);
-			await ReloadDataFromDatabase ();
-		}
+        async void OnButtonMoreClicked()
+        {
+            IsBusy = true;
+            await model.SyncNextPage(id);
+            await ReloadDataFromDatabase();
+            IsBusy = false;
+        }
 
-		async Task ReloadDataFromDatabase ()
-		{
-			var data = await model.Get (id);
-			HasNewItems = data.NewItemsCount > 0;
+        async Task ReloadDataFromDatabase()
+        {
+            var data = await model.Get(id);
+            HasNewItems = data.NewItemsCount > 0;
 
-			var newPosts = ConvertToViewModelItemList (data);
-			for (int i = Posts.Count - 1; i >= newPosts.Count; i--)
-				Posts.RemoveAt (i);
-			for (int i = Posts.Count; i < newPosts.Count; i++)
-				Posts.Add (null);
-			for (int i = 0; i < newPosts.Count; i++)
-				Posts [i] = newPosts [i];
-		}
+            var newPosts = ConvertToViewModelItemList(data);
+            for (int i = Posts.Count - 1; i >= newPosts.Count; i--)
+                Posts.RemoveAt(i);
+            for (int i = Posts.Count; i < newPosts.Count; i++)
+                Posts.Add(null);
+            for (int i = 0; i < newPosts.Count; i++)
+                Posts[i] = newPosts[i];
+        }
 
-		List<ViewModelBase> ConvertToViewModelItemList (PostCollectionState data)
-		{
-			var posts = data.Posts.Select (s => new ContentViewModel (s)).ToList<ViewModelBase> ();
-			if (data.DividerPosition >= 0) {
-				var divider = new DividerViewModel (OnButtonMoreClicked);
-				posts.Insert (data.DividerPosition, divider);
-			}
-			return posts;
-		}
+        List<ViewModelBase> ConvertToViewModelItemList(PostCollectionState data)
+        {
+            var posts = data.Posts.Select(s => new ContentViewModel(s)).ToList<ViewModelBase>();
+            if (data.DividerPosition >= 0)
+            {
+                var divider = new DividerViewModel(OnButtonMoreClicked);
+                posts.Insert(data.DividerPosition, divider);
+            }
+            return posts;
+        }
 
-		void OnChangeCurrentListId (ID newId)
-		{
-			LoadFirstPage (newId);
-		}
+        void OnChangeCurrentListId(ID newId)
+        {
+            LoadFirstPage(newId);
+        }
 
-		public class ContentViewModel : ViewModelBase
-		{
-			public RelayCommand OpenPostCommand { get; set; }
+        public class ContentViewModel : ViewModelBase
+        {
+            public RelayCommand OpenPostCommand { get; set; }
 
-			public string Title { get { return post.Title; } }
+            public string Title { get { return post.Title; } }
 
-			public string Image { get { return post.Image; } }
+            public string Image { get { return post.Image; } }
 
-			public int ImageWidth { get { return post.ImageWidth; } }
+            public int ImageWidth { get { return post.ImageWidth; } }
 
-			public int ImageHeight { get { return post.ImageHeight; } }
+            public int ImageHeight { get { return post.ImageHeight; } }
 
-			public Uri UserImage { get { return post.UserImage == null ? null : new Uri (post.UserImage); } }
+            public Uri UserImage { get { return post.UserImage == null ? null : new Uri(post.UserImage); } }
 
-			public string UserName { get { return post.UserName; } }
+            public string UserName { get { return post.UserName; } }
 
-			Post post;
+            Post post;
 
-			public ContentViewModel (Post post)
-			{
-				this.post = post;
-				OpenPostCommand = new FixRelayCommand (() =>
-					MessengerInstance.Send (new PostNavigationMessage { PostId = post.Id }));
-			}
+            public ContentViewModel(Post post)
+            {
+                this.post = post;
+                OpenPostCommand = new FixRelayCommand(() =>
+                   MessengerInstance.Send(new PostNavigationMessage { PostId = post.Id }));
+            }
 
-			public override bool Equals (object obj)
-			{
-				var o = obj as ContentViewModel;
-				return o != null && post.PostId == o.post.PostId;
-			}
-		}
+            public override bool Equals(object obj)
+            {
+                var o = obj as ContentViewModel;
+                return o != null && post.PostId == o.post.PostId;
+            }
+        }
 
-		public class DividerViewModel : ViewModelBase
-		{
-			public RelayCommand LoadMoreCommand { get; set; }
+        public class DividerViewModel : ViewModelBase
+        {
+            public RelayCommand LoadMoreCommand { get; set; }
 
-			public DividerViewModel (Action command)
-			{
-				LoadMoreCommand = new RelayCommand (command);
-			}
+            public DividerViewModel(Action command)
+            {
+                LoadMoreCommand = new RelayCommand(command);
+            }
 
-			public override bool Equals (object obj)
-			{
-				return obj is DividerViewModel;
-			}
-		}
-	}
+            public override bool Equals(object obj)
+            {
+                return obj is DividerViewModel;
+            }
+        }
+    }
 }
