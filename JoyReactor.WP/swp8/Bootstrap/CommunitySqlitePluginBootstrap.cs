65b1b0fa83018d4733ee34c25b149f6aa6c060fc
y2k <itwisterlx@gmail.com>
2014-12-18 08:21:59 +0400

    удаление ненужный файлов

--- JoyReactor.WP/swp8/Bootstrap/CommunitySqlitePluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace JoyReactor.WP.Bootstrap
-{
-    public class SqlitePluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Community.Plugins.Sqlite.PluginLoader>
-    {
-    }
-}
