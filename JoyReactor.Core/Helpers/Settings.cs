77abedb88f16e9aa2a273c883fbad6452ff528fd
y2k <itwisterlx@gmail.com>
2015-02-04 10:41:46 +0400

    настройка проекта

--- JoyReactor.Core/Helpers/Settings.cs
+++ JoyReactor.Core/Helpers/Settings.cs
@@ -1,31 +1,43 @@
+// Helpers/Settings.cs
 using Refractored.Xam.Settings;
 using Refractored.Xam.Settings.Abstractions;
 
 namespace JoyReactor.Core.Helpers
 {
-	/// <summary>
-	/// This is the Settings static class that can be used in your Core solution or in any
-	/// of your client applications. All settings are laid out the same exact way with getters
-	/// and setters. 
-	/// </summary>
-	public static class Settings
-	{
-		static ISettings AppSettings {
-			get {
-				return CrossSettings.Current;
-			}
-		}
+  /// <summary>
+  /// This is the Settings static class that can be used in your Core solution or in any
+  /// of your client applications. All settings are laid out the same exact way with getters
+  /// and setters. 
+  /// </summary>
+  public static class Settings
+  {
+    private static ISettings AppSettings
+    {
+      get
+      {
+        return CrossSettings.Current;
+      }
+    }
 
-		const string SettingsKey = "settings_key";
-		static readonly string SettingsDefault = string.Empty;
+    #region Setting Constants
 
-		public static string GeneralSettings {
-			get {
-				return AppSettings.GetValueOrDefault (SettingsKey, SettingsDefault);
-			}
-			set {
-				AppSettings.AddOrUpdateValue (SettingsKey, value);
-			}
-		}
-	}
+    private const string SettingsKey = "settings_key";
+    private static readonly string SettingsDefault = string.Empty;
+
+    #endregion
+
+
+    public static string GeneralSettings
+    {
+      get
+      {
+        return AppSettings.GetValueOrDefault(SettingsKey, SettingsDefault);
+      }
+      set
+      {
+        AppSettings.AddOrUpdateValue(SettingsKey, value);
+      }
+    }
+
+  }
 }
