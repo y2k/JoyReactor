ebeea0e851e79d95dc88b3fdece9a5a2dd8023c9
y2k <itwisterlx@gmail.com>
2015-05-02 23:24:26 +0400

    рефакторинг

---
+++ JoyReactor.Core/Model/Database/InitializeTransaction.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+
+namespace JoyReactor.Core.Model.Database
+{
+    class InitializeTransaction
+    {
+        const int DatabaseVersion = 1;
+
+        AsyncSQLiteConnection connection;
+
+        internal InitializeTransaction(AsyncSQLiteConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        internal async Task Execute()
+        {
+            int ver = await GetUserVesion(connection);
+            if (ver == 0)
+            {
+                OnCreate(connection);
+                await SetUserVersion(connection, DatabaseVersion);
+            }
+            else if (ver < DatabaseVersion)
+            {
+                // XXX: OnUpdate
+                await SetUserVersion(connection, DatabaseVersion);
+            }
+        }
+
+        async void OnCreate(AsyncSQLiteConnection db)
+        {
+            await new CreateTablesTransaction(db).Execute();
+            await new CreateDefaultTagsTransaction(db).Execute();
+        }
+
+        Task SetUserVersion(AsyncSQLiteConnection db, int version)
+        {
+            return db.ExecuteAsync("PRAGMA user_version = " + version);
+        }
+
+        Task<int> GetUserVesion(AsyncSQLiteConnection db)
+        {
+            return db.ExecuteScalarAsync<int>("PRAGMA user_version");
+        }
+    }
+}
--- JoyReactor.Core/Model/Database/SQLiteConnectionFactory.cs
+++ JoyReactor.Core/Model/Database/SQLiteConnectionFactory.cs
@@ -2,14 +2,12 @@
 using PCLStorage;
 using SQLite.Net;
 using SQLite.Net.Interop;
-using System.Threading.Tasks;
 
 namespace JoyReactor.Core.Model.Database
 {
     public class SQLiteConnectionFactory
     {
         const string DatabaseName = "net.itwister.joyreactor.main.db";
-        const int DatabaseVersion = 1;
 
         static volatile AsyncSQLiteConnection Instance;
         static object syncRoot = new object();
@@ -25,48 +23,12 @@ namespace JoyReactor.Core.Model.Database
                         var path = PortablePath.Combine(FileSystem.Current.LocalStorage.Path, DatabaseName);
                         var platform = ServiceLocator.Current.GetInstance<ISQLitePlatform>();
                         var localInstance = new AsyncSQLiteConnection(new SQLiteConnection(platform, path));
-                        InitializeDatabase(localInstance);
+                        new InitializeTransaction(localInstance).Execute();
                         Instance = localInstance;
                     }
                 }
             }
             return Instance;
-        }
-
-        internal static async void InitializeDatabase(AsyncSQLiteConnection db)
-        {
-            int ver = await GetUserVesion(db);
-            if (ver == 0)
-            {
-                OnCreate(db);
-                await SetUserVersion(db, DatabaseVersion);
-            }
-            else if (ver < DatabaseVersion)
-            {
-                OnUpdate(ver, DatabaseVersion);
-                await SetUserVersion(db, DatabaseVersion);
-            }
-        }
-
-        static async void OnCreate(AsyncSQLiteConnection db)
-        {
-            await new CreateTablesTransaction(db).Execute();
-            new CreateDefaultTagsTransaction(db).Execute();
-        }
-
-        protected static void OnUpdate(int oldVersion, int newVersion)
-        {
-            // Reserverd
-        }
-
-        static Task SetUserVersion(AsyncSQLiteConnection db, int version)
-        {
-            return db.ExecuteAsync("PRAGMA user_version = " + version);
-        }
-
-        static Task<int> GetUserVesion(AsyncSQLiteConnection db)
-        {
-            return db.ExecuteScalarAsync<int>("PRAGMA user_version");
         }
     }
 }
