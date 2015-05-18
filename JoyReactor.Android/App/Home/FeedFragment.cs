315ff9506175d817bef04fece0996457454623f3
y2k <itwisterlx@gmail.com>
2015-05-18 20:30:23 +0400

    сброс позиции списка при смене тега

--- JoyReactor.Android/App/Home/FeedFragment.cs
+++ JoyReactor.Android/App/Home/FeedFragment.cs
@@ -1,18 +1,18 @@
 ﻿using Android.OS;
 using Android.Support.V4.Widget;
-using Android.Support.V7.Widget;
 using Android.Views;
 using GalaSoft.MvvmLight.Helpers;
-using JoyReactor.Core;
-using JoyReactor.Core.ViewModels;
 using JoyReactor.Android.App.Base;
 using JoyReactor.Android.Widget;
+using JoyReactor.Core;
+using JoyReactor.Core.ViewModels;
 
 namespace JoyReactor.Android.App.Home
 {
 	public class FeedFragment : BaseFragment
 	{
 		FeedViewModel viewModel;
+        FeedRecyclerView list;
 
 		public override void OnCreate (Bundle savedInstanceState)
 		{
@@ -20,13 +20,16 @@ namespace JoyReactor.Android.App.Home
 			RetainInstance = true;
 			viewModel = new FeedViewModel ();
             viewModel.Initialize (ID.Factory.New (ID.IdConst.ReactorGood));
+
+            MessengerInstance.Register<TagsViewModel.SelectTagMessage>(this, _ => list.ResetScrollToTop());
 		}
 
 		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			var view = inflater.Inflate (Resource.Layout.fragment_feed, null);
 
-            view.FindViewById<RecyclerView> (Resource.Id.list).SetAdapter (new FeedAdapter (viewModel.Posts));
+            list = view.FindViewById<FeedRecyclerView>(Resource.Id.list);
+            list.SetAdapter (new FeedAdapter (viewModel.Posts));
 
 			var refresher = view.FindViewById<SwipeRefreshLayout> (Resource.Id.refresher);
 			refresher.SetCommand ("Refresh", viewModel.RefreshCommand);
@@ -35,7 +38,7 @@ namespace JoyReactor.Android.App.Home
 			var applyButton = view.FindViewById<ReloadButton> (Resource.Id.apply);
 			applyButton.Command = viewModel.ApplyCommand;
 			viewModel
-				.SetBinding (() => viewModel.HasNewItems, applyButton, () => applyButton.Visibility, BindingMode.OneWay)
+                .SetBinding (() => viewModel.HasNewItems, applyButton, () => applyButton.Visibility, BindingMode.OneWay)
 				.ConvertSourceToTarget (s => s ? ViewStates.Visible : ViewStates.Gone);
 
             var error = view.FindViewById(Resource.Id.error);
--- JoyReactor.Android/Widget/FeedRecyclerView.cs
+++ JoyReactor.Android/Widget/FeedRecyclerView.cs
@@ -1,16 +1,49 @@
 ﻿using Android.Support.V7.Widget;
 using Android.Content;
 using Android.Util;
+using Android.Widget;
 
 namespace JoyReactor.Android.Widget
 {
-    public class FeedRecyclerView : RecyclerView
+    public class FeedRecyclerView : FrameLayout
     {
+        RecyclerView list;
+
         public FeedRecyclerView(Context context, IAttributeSet attrs)
             : base(context, attrs)
         {
-            SetLayoutManager (new StaggeredGridLayoutManager (2, StaggeredGridLayoutManager.Vertical));
-            AddItemDecoration (new DividerItemDecoration (2.5f));
+            Initialize();
+        }
+
+        public override bool CanScrollVertically(int direction)
+        {
+            return list.CanScrollVertically(direction);
+        }
+
+        void Initialize()
+        {
+            RemoveAllViews();
+
+            list = new RecyclerView(Context);
+            list.SetLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.Vertical));
+            list.AddItemDecoration(new DividerItemDecoration(3f));
+            var padding = (int)(3 * Resources.DisplayMetrics.Density);
+            list.SetPadding(padding, padding, padding, padding);
+            list.SetClipToPadding(false);
+
+            AddView(list);
+        }
+
+        public void ResetScrollToTop()
+        {
+            var adapter = list.GetAdapter();
+            Initialize();
+            list.SetAdapter(adapter);
+        }
+
+        public void SetAdapter(RecyclerView.Adapter adapter)
+        {
+            list.SetAdapter(adapter);
         }
     }
 }
