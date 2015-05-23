3598574829f2150ad4b7eab1f97789d7cc09b9b1
y2k <itwisterlx@gmail.com>
2015-05-23 20:14:27 +0400

    feedback

--- JoyReactor.Android/Model/FeedbackController.cs
+++ JoyReactor.Android/Model/FeedbackController.cs
@@ -1,94 +1,123 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.Graphics;
-using Android.Net;
 using Android.OS;
 using Java.IO;
 using Java.Lang;
+using Java.Util.Zip;
 using File = Java.IO.File;
+using Path = System.IO.Path;
+using StringBuilder = System.Text.StringBuilder;
+using Uri = Android.Net.Uri;
 
 namespace JoyReactor.Android.Model
 {
-	class FeedbackController
-	{
-		Activity activity;
+    class FeedbackController
+    {
+        readonly Activity activity;
 
-		internal FeedbackController (Activity activity)
-		{
-			this.activity = activity;
-		}
+        internal FeedbackController(Activity activity)
+        {
+            this.activity = activity;
+        }
 
-		internal async void Send ()
-		{
-			var screenshot = await TakeScreenshot ();
-			activity.StartActivity (CreateEmailIntent (screenshot));
-		}
+        internal async void Send()
+        {
+            var path = Path.Combine("" + activity.GetExternalFilesDir(null), "system_info_" + Guid.NewGuid() + ".zip");
+            using (var file = System.IO.File.Create(path))
+            {
+                using (var zip = new ZipOutputStream(file))
+                {
+                    zip.SetMethod(ZipOutputStream.Deflated);
+                    zip.SetLevel(5);
 
-		async Task<File> TakeScreenshot ()
-		{
-			var root = activity.Window.DecorView;
-			var bitmap = Bitmap.CreateBitmap (root.Width, root.Height, Bitmap.Config.Argb8888);
-			var canvas = new Canvas (bitmap);
-			root.Draw (canvas);
+                    await zip.PutNextEntryAsync(new ZipEntry("screenshot.jpeg"));
+                    await TakeScreenshot(zip);
+                    zip.CloseEntry();
+                    await zip.PutNextEntryAsync(new ZipEntry("system_info.txt"));
+                    await zip.WriteAsync(Encoding.UTF8.GetBytes(new SystemInformation(activity).GetInformation()));
+                    zip.CloseEntry();
+                    zip.Finish();
+                }
+            }
+            activity.StartActivity(CreateEmailIntent(new File(path)));
+        }
 
-			var target = File.CreateTempFile ("feedback_screenshot_", ".jpeg", activity.GetExternalFilesDir (null));
-			using (var stream = new FileStream (target.AbsolutePath, FileMode.Create)) {
-				await bitmap.CompressAsync (Bitmap.CompressFormat.Jpeg, 90, stream);
-			}
-			return target;
-		}
+        async Task TakeScreenshot(OutputStream stream)
+        {
+            var root = activity.Window.DecorView;
+            var bitmap = Bitmap.CreateBitmap(root.Width, root.Height, Bitmap.Config.Argb8888);
+            var canvas = new Canvas(bitmap);
+            root.Draw(canvas);
 
-		Intent CreateEmailIntent (File screenshot)
-		{
-			Intent emailItent = new Intent (Intent.ActionSend)
-				.SetType ("image/jpeg")
-				.PutExtra (Intent.ExtraEmail, new [] { "feedback-y2k@gmail.com" })
-				.PutExtra (Intent.ExtraSubject, "[Feedback][JoyReactor]")
-				.PutExtra (Intent.ExtraText, GetDescription ())
-				.PutExtra (Intent.ExtraStream, Uri.FromFile (screenshot));
+            var memory = new MemoryStream();
+            await bitmap.CompressAsync(Bitmap.CompressFormat.Jpeg, 90, memory);
+            await stream.WriteAsync(memory.ToArray());
+        }
 
-			var prefApps = new [] { "com.google.android.apps.inbox", "com.google.android.gm" };
-			var package = activity.PackageManager
-				.QueryIntentActivities (emailItent, 0)
-				.Join (prefApps, s => s.ActivityInfo.PackageName, s => s, (a, b) => a)
-				.Select (s => s.ActivityInfo.PackageName)
-				.FirstOrDefault ();
-			emailItent.SetPackage (package);
-			return emailItent;
-		}
+        Intent CreateEmailIntent(File screenshot)
+        {
+            Intent emailItent = new Intent(Intent.ActionSend)
+                .SetType("application/zip")
+				.PutExtra(Intent.ExtraEmail, new [] { "itwisterlx@gmail.com" })
+				.PutExtra(Intent.ExtraSubject, "[Feedback][JoyReactor]")
+                .PutExtra(Intent.ExtraStream, Uri.FromFile(screenshot));
 
-		string GetDescription ()
-		{
-			var runtime = Runtime.GetRuntime ();
-			var desc = new Description ()
-				.Add ("Activity", activity.GetType ().Name)
-				.Add ("AvailableProcessors", runtime.AvailableProcessors ())
-				.Add ("FreeMemory", runtime.FreeMemory ())
-				.Add ("MaxMemory", runtime.MaxMemory ())
-				.Add ("TotalMemory", runtime.TotalMemory ());
-			foreach (var prop in typeof(Build).GetProperties(BindingFlags.Static | BindingFlags.Public))
-				desc.Add (prop.Name, prop.GetValue (null));
-			return desc.ToString ();
-		}
+            var prefApps = new []
+            { 
+                "com.google.android.apps.inbox", 
+                "com.google.android.gm",
+                "com.android.email"
+            };
+            var package = activity.PackageManager
+				.QueryIntentActivities(emailItent, 0)
+				.Join(prefApps, s => s.ActivityInfo.PackageName, s => s, (a, b) => a)
+				.Select(s => s.ActivityInfo.PackageName)
+				.FirstOrDefault();
+            emailItent.SetPackage(package);
+            return emailItent;
+        }
 
-		class Description
-		{
-			StringBuilder descBuilder = new StringBuilder ();
+        class SystemInformation
+        {
+            StringBuilder description = new StringBuilder();
+            Activity activity;
 
-			public Description Add (string key, object value)
-			{
-				descBuilder.Append (key + " = " + value + "\n");
-				return this;
-			}
+            public SystemInformation(Activity activity)
+            {
+                this.activity = activity;
+            }
 
-			public override string ToString ()
-			{
-				return "\n\n## system info ##\n\n" + descBuilder + "\n## system info ##\n\n";
-			}
-		}
-	}
+            public string GetInformation()
+            {
+                var runtime = Runtime.GetRuntime();
+                Add("Data", DateTime.Now);
+                Add("VersionCode", activity.PackageManager.GetPackageInfo(activity.PackageName, 0).VersionCode);
+                Add("VersionName", activity.PackageManager.GetPackageInfo(activity.PackageName, 0).VersionName);
+                Add("Activity", activity.GetType().Name);
+                Add("AvailableProcessors", runtime.AvailableProcessors());
+                Add("FreeMemory", runtime.FreeMemory());
+                Add("MaxMemory", runtime.MaxMemory());
+                Add("TotalMemory", runtime.TotalMemory());
+                description.AppendLine();
+                foreach (var prop in typeof(Build.VERSION).GetProperties(BindingFlags.Static | BindingFlags.Public))
+                    Add(prop.Name, prop.GetValue(null));
+                description.AppendLine();
+                foreach (var prop in typeof(Build).GetProperties(BindingFlags.Static | BindingFlags.Public))
+                    Add(prop.Name, prop.GetValue(null));
+                return description.ToString();
+            }
+
+            void Add(string key, object value)
+            {
+                description.Append(key + " = " + value + "\n");
+            }
+        }
+    }
 }
