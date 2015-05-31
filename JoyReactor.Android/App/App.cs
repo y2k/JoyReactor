7ddd613b1d34853f8069c29016fbc27606a32130
y2k <itwisterlx@gmail.com>
2015-05-31 13:37:39 +0400

    добавление xamarin insights

--- JoyReactor.Android/App/App.cs
+++ JoyReactor.Android/App/App.cs
@@ -2,27 +2,35 @@
 using Android.App;
 using Android.Runtime;
 using JoyReactor.Android.Model;
-using Microsoft.Practices.ServiceLocation;
 using JoyReactor.Core.Model;
+using Microsoft.Practices.ServiceLocation;
+using Xamarin;
 
 namespace JoyReactor.Android.App
 {
-    [Application (Theme = "@style/AppTheme")]
-	public class App : Application
-	{
-		public static Application Instance { get; private set; }
+    [Application(Theme = "@style/AppTheme")]
+    public class App : Application
+    {
+        public static Application Instance { get; private set; }
 
-		public App (IntPtr handle, JniHandleOwnership transfer) : base (handle, transfer)
-		{
-		}
+        public App(IntPtr handle, JniHandleOwnership transfer)
+            : base(handle, transfer)
+        {
+        }
 
-		public override void OnCreate ()
-		{
-			base.OnCreate ();
+        public override void OnCreate()
+        {
+            base.OnCreate();
 
-			Instance = this;
-			var locator = new DefaultServiceLocator (new AndroidInjectModule ());
-			ServiceLocator.SetLocatorProvider (() => locator);
-		}
-	}
+            #if DEBUG
+            Insights.Initialize("7d0715fea86f4239a3a4d6ce8acbe1871296e8a4", this);
+            #else
+            Insights.Initialize("1664e416e9def27db9e1d4ddc74255f3537a2c16", this);
+            #endif
+
+            Instance = this;
+            var locator = new DefaultServiceLocator(new AndroidInjectModule());
+            ServiceLocator.SetLocatorProvider(() => locator);
+        }
+    }
 }
