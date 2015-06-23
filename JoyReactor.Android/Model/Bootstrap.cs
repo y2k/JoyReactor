c5c5bbf38d3132ea483c1df37e1594dd4adae45a
y2k <itwisterlx@gmail.com>
2015-06-23 22:17:58 +0400

    быстро-фикс паданий в release-mode

---
+++ JoyReactor.Android/Model/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.Widget;
+using Android.Views;
+
+namespace JoyReactor.Android.Model
+{
+    public class Bootstrap
+    {
+        public Bootstrap()
+        {
+            new EditText(null).TextChanged += Stub;
+            new Button(null).Click += Stub;
+            new CheckBox(null).CheckedChange += Stub;
+            new View(null).Visibility = new View(null).Visibility;
+            new ViewAnimator(null).DisplayedChild = new ViewAnimator(null).DisplayedChild;
+        }
+
+        void Stub(object sender, EventArgs e)
+        {
+        }
+    }
+}
