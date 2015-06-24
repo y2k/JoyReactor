d6c3dd3354ffb86713278a99f21d36160f51c140
y2k <itwisterlx@gmail.com>
2015-06-24 12:36:53 +0400

    фикс парсинга рейтинга постов в списках

--- JoyReactor.Core/Model/Parser/PostCollectionRequest.cs
+++ JoyReactor.Core/Model/Parser/PostCollectionRequest.cs
@@ -220,7 +220,7 @@ namespace JoyReactor.Core.Model.Parser
                 p.PostId = PostIdRx.FirstString(html);
                 var CreatedRx = new Regex("data\\-time=\"(\\d+)\"");
                 p.Created = CreatedRx.FirstLong(html) * 1000L;
-                var RatingRx = new Regex("Рейтинг:\\s*<div class=\"[^\"]+\"></div>\\s*([\\d\\.]+)");
+                var RatingRx = new Regex("class=\"post_rating\"><span>([^<]+)");
                 p.Rating = RatingRx.FirstFloat(html, CultureInfo.InvariantCulture);
 
                 var CoubRx = new Regex("<iframe src=\"http://coub.com/embed/(.+?)\" allowfullscreen=\"true\" frameborder=\"0\" width=\"(\\d+)\" height=\"(\\d+)");
