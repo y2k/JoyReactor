835fb3ce275efe77fb7f06d8dc233b3e7ce1f1b6
y2k <itwisterlx@gmail.com>
2015-09-30 18:29:27 +0400

    рефакторинг

--- core/src/main/java/y2k/joyreactor/Profile.java
+++ core/src/main/java/y2k/joyreactor/Profile.java
@@ -1,9 +1,9 @@
 package y2k.joyreactor;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import rx.Observable;
 
-import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,16 +29,7 @@ public class Profile {
     static Observable<Profile> request(String username) {
         return new HttpClient()
                 .getDocumentAsync(getUrl(username))
-                .map(doc -> {
-                    ProfileParser parser = new ProfileParser(doc);
-                    Profile profile = new Profile();
-                    profile.userName = username;
-                    profile.userImage = parser.getUserImage();
-                    profile.progressToNewStar = parser.getProgressToNewStar();
-                    profile.rating = parser.getRating();
-                    profile.stars = parser.getStars();
-                    return profile;
-                });
+                .map(document -> new ProfileParser(document).parse());
     }
 
     private static String getUrl(String username) {
@@ -53,23 +44,25 @@ public class Profile {
             this.document = document;
         }
 
-        String getUserImage() {
-            return document.select("img.avatar").first().attr("src");
+        Profile parse() {
+            Profile profile = new Profile();
+            profile.userName = getAvatarNode().attr("alt");
+            profile.userImage = getAvatarNode().attr("src");
+            profile.progressToNewStar = getProgressToNewStar();
+            profile.rating = Float.parseFloat(document.select("#rating-text > b").text());
+            profile.stars = document.select(".star-row-0 > .star-0").size();
+            return profile;
         }
 
-        float getProgressToNewStar() {
+        private Element getAvatarNode() {
+            return document.select("img.avatar").first();
+        }
+
+        private float getProgressToNewStar() {
             String style = document.select("div.poll_res_bg_active").first().attr("style");
             Matcher m = Pattern.compile("width:(\\d+)%;").matcher(style);
             if (!m.find()) throw new IllegalStateException();
             return Float.parseFloat(m.group(1));
-        }
-
-        float getRating() {
-            return Float.parseFloat(document.select("#rating-text > b").text());
-        }
-
-        int getStars() {
-            return document.select(".star-row-0 > .star-0").size();
         }
     }
 }
