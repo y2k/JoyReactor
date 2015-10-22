d06b733536a83f4c61e2d1465293ddc5ce1ea592
y2k <itwisterlx@gmail.com>
2015-10-22 20:31:28 +0400

    отображение видео

--- ios/src/main/java/y2k/joyreactor/VideoViewController.java
+++ ios/src/main/java/y2k/joyreactor/VideoViewController.java
@@ -3,6 +3,9 @@ package y2k.joyreactor;
 import org.robovm.apple.avfoundation.AVLayerVideoGravity;
 import org.robovm.apple.avfoundation.AVPlayer;
 import org.robovm.apple.avfoundation.AVPlayerLayer;
+import org.robovm.apple.coremedia.CMTime;
+import org.robovm.apple.dispatch.DispatchQueue;
+import org.robovm.apple.foundation.NSObject;
 import org.robovm.apple.foundation.NSURL;
 import org.robovm.apple.uikit.UIActivityIndicatorView;
 import org.robovm.apple.uikit.UIViewController;
@@ -11,6 +14,7 @@ import org.robovm.objc.annotation.IBOutlet;
 import y2k.joyreactor.presenters.VideoPresenter;
 
 import java.io.File;
+import java.util.Collections;
 
 /**
  * Created by y2k on 22/10/15.
@@ -19,6 +23,9 @@ import java.io.File;
 public class VideoViewController extends UIViewController implements VideoPresenter.View {
 
     UIActivityIndicatorView indicatorView;
+    //    AVPlayer player;
+    NSObject repeatObserver;
+    AVPlayer player;
 
     @Override
     public void viewDidLoad() {
@@ -30,19 +37,42 @@ public class VideoViewController extends UIViewController implements VideoPresen
     public void viewWillAppear(boolean animated) {
         super.viewWillAppear(animated);
         getNavigationController().setHidesBarsOnTap(true);
+
+//        NSNotificationCenter.getDefaultCenter().addObserver(
+//                AVPlayerItem.DidPlayToEndTimeNotification(), this, null, s -> {
+//                    player.seekToTime(CMTime.Zero());
+//                });
+
+//        NSNotificationCenter.getDefaultCenter().addObserver(this, Selector.register(""), null, player.addPeriodicTimeObserver());
+
+//        player.addBoundaryTimeObserver()
+
+//        player.getCurrentItem().getDuration()
     }
 
     @Override
     public void viewWillDisappear(boolean animated) {
         super.viewWillDisappear(animated);
         getNavigationController().setHidesBarsOnTap(false);
+//        NSNotificationCenter.getDefaultCenter().removeObserver(this);
+        if (repeatObserver != null) player.removeTimeObserver(repeatObserver);
     }
 
     @Override
     public void showVideo(File videoFile) {
-        AVPlayer player = new AVPlayer(new NSURL(videoFile));
-        AVPlayerLayer layer = new AVPlayerLayer();
+        player = new AVPlayer(new NSURL(videoFile));
 
+        System.out.println("Length = " + player.getCurrentItem().getAsset().getDuration().getSeconds());
+        repeatObserver = player.addBoundaryTimeObserver(
+                Collections.singletonList(player.getCurrentItem().getAsset().getDuration().subtract(CMTime.create(0.1, 600))),
+                null,
+                () -> {
+                    System.out.println("Video ended");
+                    player.seekToTime(CMTime.Zero());
+//                    player.play();
+                });
+
+        AVPlayerLayer layer = new AVPlayerLayer();
         layer.setPlayer(player);
         layer.setFrame(getView().getFrame());
         layer.setVideoGravity(AVLayerVideoGravity.ResizeAspect);
