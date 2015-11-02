366ad9f49ff8ae466a15a4f4ae23ec09c87fc1ef
y2k <itwisterlx@gmail.com>
2015-11-02 14:20:38 +0400

    тесты

--- core/src/test/java/y2k/joyreactor/presenters/PostListPresenterTest.java
+++ core/src/test/java/y2k/joyreactor/presenters/PostListPresenterTest.java
@@ -2,19 +2,20 @@ package y2k.joyreactor.presenters;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import rx.Observable;
 import y2k.joyreactor.Post;
 import y2k.joyreactor.Repository;
 import y2k.joyreactor.requests.PostsForTagRequest;
 
-import java.util.ArrayList;
+import java.util.*;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -22,26 +23,36 @@ import static org.mockito.Mockito.when;
  */
 public class PostListPresenterTest {
 
+    @Mock
+    PostListPresenter.View mockView;
+    @Mock
+    Repository<Post> mockRepository;
+    @Mock
+    PostsForTagRequest mockRequest;
+    @Mock
+    PostsForTagRequest.Factory mockFactory;
+
+    @Captor
+    ArgumentCaptor<List<Post>> captor;
+
     @Before
     public void setUp() throws Exception {
-        PostListPresenter.View mockView = mock(PostListPresenter.View.class);
+        MockitoAnnotations.initMocks(this);
 
-        Repository<Post> mockRepository = mock(Repository.class);
         when(mockRepository.queryAsync()).thenReturn(Observable.just(new ArrayList<>()));
         when(mockRepository.replaceAllAsync(any())).thenReturn(Observable.just(null));
 
-        PostsForTagRequest mockRequest = mock(PostsForTagRequest.class);
-        when(mockRequest.getPosts()).thenReturn(new ArrayList<>());
+        when(mockRequest.getPosts()).thenReturn(Arrays.asList(new Post[10]));
         when(mockRequest.requestAsync()).thenReturn(Observable.just(null));
 
-        PostsForTagRequest.Factory mockFactory = mock(PostsForTagRequest.Factory.class);
         when(mockFactory.make(anyString(), anyString())).thenReturn(mockRequest);
-
-        new PostListPresenter(mockView, mockRepository, mockFactory);
     }
 
     @Test
-    public void testApplyNew() throws Exception {
-        // TODO:
+    public void testConstructor() throws Exception {
+        new PostListPresenter(mockView, mockRepository, mockFactory);
+
+        verify(mockView).reloadPosts(captor.capture(), isNull(Integer.class));
+        assertEquals(0, captor.getValue().size());
     }
 }
