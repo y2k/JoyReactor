d9f615e8567c1387f81fc97f415b02b98ab40b89
y2k <itwisterlx@gmail.com>
2015-11-07 03:48:27 +0400

    перевод утилиты загрузки тегов на coreclr

--- _web/TagIconGrabber/Program.cs
+++ _web/TagIconGrabber/Program.cs
@@ -1,8 +1,7 @@
 using System.IO;
 using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Text.RegularExpressions;
-using System.Web;
 
 public class Program
 {
@@ -11,7 +10,7 @@ public class Program
         var tags = Enumerable
             .Range(1, 20)
             .Select(s => s == 1 ? "" : "/" + s)
-            .Select(s => new WebClient().DownloadString("http://joyreactor.cc/tags/subscribers" + s))
+            .Select(s => new HttpClient().GetStringAsync("http://joyreactor.cc/tags/subscribers" + s).Result)
             .Select(s => Regex.Matches(s, "<img src=\"[^\"]+/tag/(\\d+)\" alt=\"([^\"]+)\"/>"))
             .SelectMany(s => s.OfType<Match>())
             .Select(s => new { key = Decode(s), value = s.Groups[1].Value})
@@ -26,6 +25,6 @@ public class Program
 
     static string Decode(Match s)
     {
-        return HttpUtility.HtmlDecode(s.Groups[2].Value.ToLower()).Replace("\"", "\\\"");
+        return System.Net.WebUtility.HtmlDecode(s.Groups[2].Value.ToLower()).Replace("\"", "\\\"");
     }
 }
