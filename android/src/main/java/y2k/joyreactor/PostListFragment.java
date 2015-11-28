811af52536e5279b03d9f4172ca6ff6517dc7b2c
y2k <itwisterlx@gmail.com>
2015-11-28 21:32:25 +0400

    android ui правки

--- android/src/main/java/y2k/joyreactor/PostListFragment.java
+++ android/src/main/java/y2k/joyreactor/PostListFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import org.ocpsoft.prettytime.PrettyTime;
+import y2k.joyreactor.common.ItemDividerDecoration;
 import y2k.joyreactor.platform.ImageRequest;
 import y2k.joyreactor.presenters.PostListPresenter;
 
@@ -15,7 +18,7 @@ import java.util.List;
 /**
  * Created by y2k on 9/26/15.
  */
-public class PostListFragment extends Fragment implements PostListPresenter.View {
+public class PostListFragment extends Fragment {
 
     PostListPresenter presenter;
     PostAdapter adapter;
@@ -29,27 +32,28 @@ public class PostListFragment extends Fragment implements PostListPresenter.View
         RecyclerView list = (RecyclerView) view.findViewById(R.id.list);
         list.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         list.setAdapter(adapter = new PostAdapter());
+        list.addItemDecoration(new ItemDividerDecoration(list));
 
-        presenter = new PostListPresenter(this);
+        presenter = new PostListPresenter(new PostListPresenter.View() {
+
+            @Override
+            public void setBusy(boolean isBusy) {
+                // TODO:
+            }
+
+            @Override
+            public void reloadPosts(List<Post> posts, Integer divider) {
+                adapter.reloadData(posts);
+            }
+
+            @Override
+            public void setHasNewPosts(boolean hasNewPosts) {
+                ((ReloadButton) getView().findViewById(R.id.apply)).setVisibility(hasNewPosts);
+            }
+        });
 
         view.findViewById(R.id.apply).setOnClickListener(v -> presenter.applyNew());
-
         return view;
-    }
-
-    @Override
-    public void setBusy(boolean isBusy) {
-        // TODO:
-    }
-
-    @Override
-    public void reloadPosts(List<Post> posts, Integer divider) {
-        adapter.reloadData(posts);
-    }
-
-    @Override
-    public void setHasNewPosts(boolean hasNewPosts) {
-        ((ReloadButton) getView().findViewById(R.id.apply)).setVisibility(hasNewPosts);
     }
 
     @Override
@@ -66,6 +70,7 @@ public class PostListFragment extends Fragment implements PostListPresenter.View
 
     class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostViewHolder> {
 
+        private PrettyTime prettyTime = new PrettyTime();
         private List<Post> posts;
 
         @Override
@@ -94,6 +99,9 @@ public class PostListFragment extends Fragment implements PostListPresenter.View
 
             h.userImage.setImage(i.getUserImage().toImage());
             h.videoMark.setVisibility(image != null && image.isAnimated() ? View.VISIBLE : View.GONE);
+
+            h.commentCount.setText("" + i.commentCount);
+            h.time.setText(prettyTime.format(i.created));
         }
 
         @Override
@@ -112,6 +120,8 @@ public class PostListFragment extends Fragment implements PostListPresenter.View
             WebImageView image;
             WebImageView userImage;
             View videoMark;
+            TextView commentCount;
+            TextView time;
 
             public PostViewHolder(View view) {
                 super(view);
@@ -120,6 +130,8 @@ public class PostListFragment extends Fragment implements PostListPresenter.View
                 imagePanel = (FixedAspectPanel) view.findViewById(R.id.imagePanel);
                 userImage = (WebImageView) view.findViewById(R.id.userImage);
                 videoMark = view.findViewById(R.id.videoMark);
+                commentCount = (TextView) view.findViewById(R.id.commentCount);
+                time = (TextView) view.findViewById(R.id.time);
 
                 view.findViewById(R.id.card).setOnClickListener(
                         v -> presenter.postClicked(posts.get(getAdapterPosition())));
