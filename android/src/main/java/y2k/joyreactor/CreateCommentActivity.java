ad31d9260ecdb95009c638ae3b3af9b2f5b70acd
y2k <itwisterlx@gmail.com>
2015-12-04 20:51:41 +0400

    (android) create comment UI

--- android/src/main/java/y2k/joyreactor/CreateCommentActivity.java
+++ android/src/main/java/y2k/joyreactor/CreateCommentActivity.java
@@ -2,8 +2,6 @@ package y2k.joyreactor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 import y2k.joyreactor.presenters.CreateCommentPresenter;
 
@@ -14,6 +12,9 @@ public class CreateCommentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_comment);
 
+        TextView nameView = (TextView) findViewById(R.id.userName);
+        TextView textView = (TextView) findViewById(R.id.text);
+
         CreateCommentPresenter presenter = new CreateCommentPresenter(new CreateCommentPresenter.View() {
 
             @Override
@@ -23,35 +24,14 @@ public class CreateCommentActivity extends AppCompatActivity {
 
             @Override
             public void setUser(Profile profile) {
-                // TODO:
+                ((WebImageView) findViewById(R.id.userImage)).setImage(profile.userImage);
+
+                nameView.setText(profile.userName);
+                nameView.setAlpha(0);
+                nameView.animate().alpha(1);
             }
         });
 
-        TextView text = (TextView) findViewById(R.id.text);
-        findViewById(R.id.send).setOnClickListener(v -> {
-            presenter.create("" + text.getText());
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_comment, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        findViewById(R.id.send).setOnClickListener(v -> presenter.create("" + textView.getText()));
     }
 }
