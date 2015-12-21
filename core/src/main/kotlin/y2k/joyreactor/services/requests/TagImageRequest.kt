894ea26e4929a553626bab3298ec232e309b0a1c
y2k <itwisterlx@gmail.com>
2015-12-21 17:24:24 +0400

    kotlin

---
+++ core/src/main/kotlin/y2k/joyreactor/services/requests/TagImageRequest.kt
@@ -0,0 +1,47 @@
+package y2k.joyreactor.services.requests
+
+import y2k.joyreactor.common.PersistentMap
+import y2k.joyreactor.http.HttpClient
+import java.io.IOException
+import java.net.URLEncoder
+
+/**
+ * Created by y2k on 10/18/15.
+ */
+class TagImageRequest {
+
+    private val cache = PersistentMap("tag-images.1.dat")
+
+    fun request(tag: String): String {
+        sStorage = IconStorage.get(sStorage, "tag.names", "tag.icons")
+
+        val clearTag = tag.toLowerCase()
+        var imageId = getImageId(clearTag)
+
+        if (imageId == null) imageId = cache.get(clearTag)
+        if (imageId == null) {
+            imageId = getFromWeb(clearTag)
+            if (imageId != null) cache.put(clearTag, imageId).flush()
+        }
+
+        if (imageId == null) throw IllegalStateException()
+        return imageId
+    }
+
+    private fun getImageId(clearTag: String): String? {
+        val id = sStorage!!.getImageId(clearTag)
+        return if (id == null) null else "http://img0.reactor.cc/pics/avatar/tag/" + id
+    }
+
+    private fun getFromWeb(tag: String): String {
+        val doc = HttpClient.getInstance().getDocument("http://joyreactor.cc/tag/" + URLEncoder.encode(tag))
+        val result = doc.select("img.blog_avatar").first().attr("src")
+        println("Not found in cache | $tag | $result")
+        return result
+    }
+
+    companion object {
+
+        private var sStorage: IconStorage? = null
+    }
+}
