f1b7db4c2e1871e89e84a291597c0c3d87d4d7d9
y2k <itwisterlx@gmail.com>
2015-12-21 15:42:56 +0400

    kotlin

---
+++ core/src/main/kotlin/y2k/joyreactor/UserImage.kt
@@ -0,0 +1,30 @@
+package y2k.joyreactor
+
+/**
+ * Created by y2k on 10/13/15.
+ */
+class UserImage(url: String? = null) {
+
+    private var userImage: String? = null
+
+    init {
+        if (url == null)
+            userImage = APP_DEFAULT_USER_IMAGE
+        else
+            this.userImage = if (SITE_DEFAULT_USER_IMAGE == url) APP_DEFAULT_USER_IMAGE else url
+    }
+
+    fun toImage(): Image {
+        return Image(userImage, 0, 0)
+    }
+
+    companion object {
+
+        private val SITE_DEFAULT_USER_IMAGE = "http://img0.joyreactor.cc/images/default_avatar.jpeg"
+        private val APP_DEFAULT_USER_IMAGE = "https://raw.githubusercontent.com/y2k/JoyReactor/master/ios/resources/Images.xcassets/AppIcon.appiconset/Icon-60%403x.png"
+
+        fun fromUrl(url: String?): UserImage {
+            return if (url == null) UserImage() else UserImage(url)
+        }
+    }
+}
