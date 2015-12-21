4f17304c62395763c6d229117fa6c0b9297a7eeb
y2k <itwisterlx@gmail.com>
2015-12-21 20:33:09 +0400

    kotlin

---
+++ core/src/main/kotlin/y2k/joyreactor/services/requests/LoginRequestFactory.kt
@@ -0,0 +1,35 @@
+package y2k.joyreactor.services.requests
+
+import rx.Observable
+import y2k.joyreactor.common.ObservableUtils
+import y2k.joyreactor.http.HttpClient
+import java.io.IOException
+
+/**
+ * Created by y2k on 9/30/15.
+ */
+class LoginRequestFactory {
+
+    fun request(username: String, password: String): Observable<Void> {
+        return ObservableUtils.action {
+            val doc = HttpClient.getInstance()
+                    .beginForm()
+                    .put("signin[username]", username)
+                    .put("signin[password]", password)
+                    .put("signin[remember]", "on")
+                    .put("signin[_csrf_token]", getCsrf())
+                    .send("http://joyreactor.cc/login")
+
+            //            System.out.println(doc.html());
+
+            // FIXME:
+            //            if (doc.getElementById("logout") == null)
+            //                throw new IllegalStateException();
+        }
+    }
+
+    private fun getCsrf(): String {
+        val document = HttpClient.getInstance().getDocument("http://joyreactor.cc/login")
+        return document.getElementById("signin__csrf_token").attr("value")
+    }
+}
