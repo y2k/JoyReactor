edfc5609eb592ca53bb301f05b96869ea3dd2d95
y2k <itwisterlx@gmail.com>
2015-12-21 17:29:27 +0400

    kotlin

---
+++ core/src/main/kotlin/y2k/joyreactor/services/requests/AddTagRequest.kt
@@ -0,0 +1,21 @@
+package y2k.joyreactor.services.requests
+
+import rx.Observable
+import y2k.joyreactor.common.ObservableUtils
+import y2k.joyreactor.http.HttpClient
+import java.net.URLEncoder
+
+/**
+ * Created by y2k on 19/10/15.
+ */
+class AddTagRequest(private val tagName: String) {
+
+    fun request(): Observable<Void> {
+        return ObservableUtils.action {
+            val tagUrl = "http://joyreactor.cc/tag/" + URLEncoder.encode(tagName)
+            val tagPage = HttpClient.getInstance().getDocument(tagUrl)
+            val addTagLink = tagPage.select("a.change_favorite_link").first().absUrl("href")
+            HttpClient.getInstance().getText(addTagLink)
+        }
+    }
+}
