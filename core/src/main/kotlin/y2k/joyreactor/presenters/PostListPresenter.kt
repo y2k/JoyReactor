dd25850bc411a32d3c8e5df0b7d54a20851df71d
y2k <itwisterlx@gmail.com>
2015-12-24 10:26:39 +0400

    рефакторинг

--- core/src/main/kotlin/y2k/joyreactor/presenters/PostListPresenter.kt
+++ core/src/main/kotlin/y2k/joyreactor/presenters/PostListPresenter.kt
@@ -29,7 +29,7 @@ class PostListPresenter(
 
         service.preloadNewPosts().subscribe(
                 { unsafeUpdate ->
-                    view.setHasNewPosts(unsafeUpdate!!)
+                    view.setHasNewPosts(unsafeUpdate)
                     view.setBusy(false)
                     if ((!unsafeUpdate)) applyNew()
                 }, { it.printStackTrace() })
