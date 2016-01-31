28042ab0aabad77f2d64663f1c608b0090bf6873
y2k <itwisterlx@gmail.com>
2016-01-31 23:04:48 +0400

    рефакторинг

---
+++ core/src/main/kotlin/y2k/joyreactor/common/PartialResult.kt
@@ -0,0 +1,19 @@
+package y2k.joyreactor.common
+
+/**
+ * Created by y2k on 08/12/15.
+ */
+class PartialResult<T>(
+    val result: T?, val progress: Int, val max: Int) {
+
+    companion object {
+
+        fun <T> complete(data: T): PartialResult<T> {
+            return PartialResult(data, 0, 0)
+        }
+
+        fun <T> inProgress(progress: Int, max: Int): PartialResult<T> {
+            return PartialResult(null, progress, max)
+        }
+    }
+}
