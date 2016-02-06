eaf69dbe885577386f2ad547ee92141988088250
y2k <itwisterlx@gmail.com>
2016-02-06 18:53:57 +0400

    временное отключение поддержки TV снижение мин версии до 14

--- android/src/main/kotlin/android/support/v17/leanback/app/OnHeaderViewSelectedListenerImpl.kt
+++
@@ -1,6 +0,0 @@
-package android.support.v17.leanback.app
-
-/**
- * Created by y2k on 11/25/15.
- */
-internal interface OnHeaderViewSelectedListenerImpl : HeadersFragment.OnHeaderViewSelectedListener
