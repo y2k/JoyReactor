7eeb09a950ab599d99c9af466a9c3871e337cee3
y2k <itwisterlx@gmail.com>
2016-02-08 14:25:00 +0400

    fix iOS build

--- ios/src/main/java/y2k/joyreactor/MenuViewController.java
+++ ios/src/main/java/y2k/joyreactor/MenuViewController.java
@@ -1,10 +1,12 @@
 package y2k.joyreactor;
 
 import kotlin.Unit;
+import org.jetbrains.annotations.NotNull;
 import org.robovm.apple.foundation.NSIndexPath;
 import org.robovm.apple.uikit.*;
 import org.robovm.objc.annotation.CustomClass;
 import org.robovm.objc.annotation.IBOutlet;
+import y2k.joyreactor.common.BaseUIViewController;
 import y2k.joyreactor.common.ServiceLocator;
 import y2k.joyreactor.platform.ImageRequest;
 import y2k.joyreactor.presenters.TagListPresenter;
@@ -15,7 +17,7 @@ import java.util.List;
  * Created by y2k on 9/26/15.
  */
 @CustomClass("MenuViewController")
-public class MenuViewController extends UIViewController implements TagListPresenter.View {
+public class MenuViewController extends BaseUIViewController {
 
     UITableView list;
     List<? extends Tag> tags;
@@ -25,21 +27,17 @@ public class MenuViewController extends UIViewController implements TagListPrese
     public void viewDidLoad() {
         super.viewDidLoad();
 
-        presenter = ServiceLocator.INSTANCE.resolve(this);
+        presenter = ServiceLocator.INSTANCE.resolve(getLifeCycleService(), new TagListPresenter.View() {
+
+            @Override
+            public void reloadData(@NotNull List<Tag> tags) {
+                MenuViewController.this.tags = tags;
+                list.reloadData();
+            }
+        });
+
         list.setDataSource(new TagDataSource());
         list.setDelegate(new TagDelegate());
-    }
-
-    @Override
-    public void viewWillAppear(boolean animated) {
-        super.viewWillAppear(animated);
-        presenter.activate();
-    }
-
-    @Override
-    public void reloadData(List<? extends Tag> tags) {
-        this.tags = tags;
-        list.reloadData();
     }
 
     @IBOutlet
@@ -67,12 +65,12 @@ public class MenuViewController extends UIViewController implements TagListPrese
                 UIImageView iv = (UIImageView) cell.getViewWithTag(1);
                 iv.getLayer().setCornerRadius(iv.getFrame().getWidth() / 2);
                 new ImageRequest()
-                        .setUrl(i.getImage())
-                        .setSize(40, 40)
-                        .to(iv, uiImage -> {
-                            iv.setImage(uiImage);
-                            return Unit.INSTANCE;
-                        });
+                    .setUrl(i.getImage())
+                    .setSize(40, 40)
+                    .to(iv, uiImage -> {
+                        iv.setImage(uiImage);
+                        return Unit.INSTANCE;
+                    });
                 ((UILabel) cell.getViewWithTag(2)).setText(i.getTitle());
                 return cell;
             }
