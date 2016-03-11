fb3a6afaa99d5ea6e82204e66f7d15eeb874a04a
y2k <itwisterlx@gmail.com>
2016-03-11 10:22:07 +0400

    fix iOS build

--- ios/src/main/kotlin/y2k/joyreactor/CreateCommentViewController.kt
+++ ios/src/main/kotlin/y2k/joyreactor/CreateCommentViewController.kt
@@ -6,8 +6,8 @@ import org.robovm.apple.uikit.UIViewController
 import org.robovm.objc.annotation.CustomClass
 import org.robovm.objc.annotation.IBOutlet
 import y2k.joyreactor.common.ServiceLocator
-import y2k.joyreactor.model.Profile
-import y2k.joyreactor.presenters.CreateCommentPresenter
+import y2k.joyreactor.common.bindingBuilder
+import y2k.joyreactor.viewmodel.CreateCommentViewModel
 
 /**
  * Created by y2k on 10/4/15.
@@ -22,20 +22,12 @@ class CreateCommentViewController : UIViewController() {
     override fun viewDidLoad() {
         super.viewDidLoad()
 
-        val presenter = ServiceLocator.resolve(
-            object : CreateCommentPresenter.View {
-
-                override fun setIsBusy(isBusy: Boolean) {
-                    navigationItem.setHidesBackButton(isBusy, true)
-                }
-
-                override fun setUser(profile: Profile) {
-                    // TODO
-                }
-            }
-        )
-
-        cancelButton.addOnTouchUpInsideListener { sender, e -> navigationController.popViewController(true) }
-        sendButton.addOnTouchUpInsideListener { sender, e -> presenter.create(commentTextView.text) }
+        val vm = ServiceLocator.resolve<CreateCommentViewModel>()
+        bindingBuilder {
+            command(sendButton) { vm.create() }
+            command(cancelButton) { navigationController.popViewController(true) }
+            action(vm.isBusy) { navigationItem.setHidesBackButton(it, true) }
+            textView(commentTextView, vm.commentText)
+        }
     }
 }
