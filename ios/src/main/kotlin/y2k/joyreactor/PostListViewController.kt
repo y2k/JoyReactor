ec288d5ffe92bd572ee1282e6e988b011d545e8b
y2k <itwisterlx@gmail.com>
2016-03-13 00:57:15 +0400

    refactoring

--- ios/src/main/kotlin/y2k/joyreactor/PostListViewController.kt
+++ ios/src/main/kotlin/y2k/joyreactor/PostListViewController.kt
@@ -21,7 +21,6 @@ class PostListViewController : BaseUIViewController() {
     @IBOutlet lateinit var list: UITableView
     @IBOutlet lateinit var progressView: UIActivityIndicatorView
     @IBOutlet lateinit var applyButton: UIButton
-    //    lateinit var refresher: UIRefreshControl
 
     override fun viewDidLoad() {
         super.viewDidLoad()
@@ -56,10 +55,6 @@ class PostListViewController : BaseUIViewController() {
                 command { vm.itemSelected(it) }
             }
         }
-
-        //        list.dataSource = PostDataSource()
-        //        list.setDelegate(PostDelegate())
-        //        progressView.stopAnimating()
     }
 
     @CustomClass("LoadMoreCell")
@@ -85,9 +80,6 @@ class PostListViewController : BaseUIViewController() {
         @IBOutlet lateinit var root: UIView
 
         @IBOutlet lateinit var height: NSLayoutConstraint
-
-        //        private lateinit var presenter: PostListPresenter
-        //        private lateinit var post: Post
 
         lateinit var vm: PostListViewModel
         lateinit var post: Post
@@ -120,8 +112,6 @@ class PostListViewController : BaseUIViewController() {
             playButton.isHidden = data.image == null
             playButton.setTitle(getPlayButtonTitle(data), UIControlState.Normal)
 
-            // ==================
-
             userName.text = post.userName
             created.text = PrettyTime().format(post.created)
 
@@ -136,7 +126,6 @@ class PostListViewController : BaseUIViewController() {
             loadImage(post.getUserImage2().toImage(), 50, 50, userImage)
             userImage.layer.cornerRadius = userImage.frame.width / 2
 
-            //            val root = cell.getViewWithTag(10)
             root.layer.cornerRadius = 10.0
             root.layer.setMasksToBounds(true)
             root.layer.borderColor = UIColor.lightGray().cgColor
@@ -158,82 +147,4 @@ class PostListViewController : BaseUIViewController() {
             return if (post.image?.isAnimated ?: false) "Play".translate() else "View".translate()
         }
     }
-
-    //    // ==========================================
-    //    // Implement View methods
-    //    // ==========================================
-    //
-    //    override fun reloadPosts(posts: List<Post>, divider: Int?) {
-    //        this.posts = posts
-    //        list.reloadData()
-    //    }
-    //
-    //    internal inner class PostDataSource : UITableViewDataSourceAdapter() {
-    //
-    //        override fun getNumberOfRowsInSection(tableView: UITableView?, section: Long): Long {
-    //            val count = if (posts == null) 0 else posts!!.size
-    //            return (if (count == 0) 0 else count + 1).toLong()
-    //        }
-    //
-    //        override fun getCellForRow(tableView: UITableView?, indexPath: NSIndexPath?): UITableViewCell {
-    //            if (indexPath!!.row == posts!!.size) {
-    //                val cell = tableView!!.dequeueReusableCell("LoadMore") as LoadMoreCell
-    //                cell.presenter = presenter
-    //                return cell
-    //            } else {
-    //                val cell = tableView!!.dequeueReusableCell("Post") as PostCell
-    //                val post = posts!![indexPath.row]
-    //                cell.update(presenter, post)
-    //
-    //                (cell.getViewWithTag(3) as UILabel).text = post.userName
-    //                (cell.getViewWithTag(4) as UILabel).text = PrettyTime().format(post.created)
-    //
-    //                val image = post.image
-    //                if (image == null) {
-    //                    // TODO
-    //                } else {
-    //                    loadImage(post.image!!, 300,
-    //                        (300 / image.aspect).toInt(),
-    //                        cell.getViewWithTag(1) as UIImageView)
-    //                }
-    //
-    //                val userImageView = cell.getViewWithTag(2) as UIImageView
-    //                loadImage(post.getUserImage2().toImage(), 50, 50, userImageView)
-    //                userImageView.layer.cornerRadius = userImageView.frame.width / 2
-    //
-    //                val root = cell.getViewWithTag(10)
-    //                root.layer.cornerRadius = 10.0
-    //                root.layer.setMasksToBounds(true)
-    //                root.layer.borderColor = UIColor.lightGray().cgColor
-    //                root.layer.borderWidth = 1.0
-    //                return cell
-    //            }
-    //        }
-    //
-    //        fun loadImage(image: Image, width: Int, height: Int, iv: UIImageView) {
-    //            iv.alpha = 0.0
-    //            ImageRequest()
-    //                .setUrl(image)
-    //                .setSize(width, height)
-    //                .to(iv) { data ->
-    //                    iv.image = data
-    //                    UIView.animate(0.3) { iv.alpha = 1.0 }
-    //                }
-    //        }
-    //    }
-    //
-    //    internal inner class PostDelegate : UITableViewDelegateAdapter() {
-    //
-    //        override fun getHeightForRow(tableView: UITableView?, indexPath: NSIndexPath?): Double {
-    //            if (indexPath!!.row == posts!!.size) return -1.0
-    //            val post = posts!![indexPath.row]
-    //            val image = post.image
-    //            val imageHeight = if (image == null) 0.0 else (tableView!!.frame.width - 16) / image.aspect
-    //            return imageHeight + 66.0 + 16.0
-    //        }
-    //
-    //        override fun didEndDecelerating(scrollView: UIScrollView?) {
-    //            if (refresher.isRefreshing) presenter.reloadFirstPage()
-    //        }
-    //    }
 }
