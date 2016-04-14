a9091391424132c0021476899ba488ea89942ced
y2k <itwisterlx@gmail.com>
2016-04-14 12:07:16 +0400

    send feedback via telegram

--- android/src/main/kotlin/y2k/joyreactor/platform/ReportService.kt
+++ android/src/main/kotlin/y2k/joyreactor/platform/ReportService.kt
@@ -1,7 +1,9 @@
 package y2k.joyreactor.platform
 
 import android.content.Intent
+import android.net.Uri
 import y2k.joyreactor.App
+import y2k.joyreactor.BuildConfig
 import y2k.joyreactor.R
 
 /**
@@ -12,10 +14,20 @@ class ReportService {
     private val context = App.instance
 
     fun createFeedback() {
-        val intent = Intent(Intent.ACTION_SEND)
+        val emailIntent = createEmailIntent() ?: return
+        val telegramIntent = createTelegramIntent()
+
+        val chooser = Intent
+            .createChooser(emailIntent, context.getString(R.string.send_feedback_by))
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(telegramIntent))
+        context.startActivity(chooser)
+    }
+
+    private fun createEmailIntent(): Intent? {
+        val emailIntent = Intent(Intent.ACTION_SEND)
             .putExtra(Intent.EXTRA_EMAIL, arrayOf("joyreactor.feedbacks@gmail.com"))
             .putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.write_feedback))
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             .setType("text/html")
 
         val prefEmailClients = arrayOf(
@@ -24,11 +36,16 @@ class ReportService {
             "com.android.email")
 
         val packageName = context.packageManager
-            .queryIntentActivities(intent, 0)
+            .queryIntentActivities(emailIntent, 0)
             .map { it.activityInfo.packageName }
             .firstOrNull { prefEmailClients.contains(it) }
-        intent.setPackage(packageName)
+        emailIntent.setPackage(packageName)
+        return emailIntent
+    }
 
-        context.startActivity(intent)
+    private fun createTelegramIntent(): Intent {
+        return Intent(Intent.ACTION_VIEW)
+            .setData(Uri.parse("https://telegram.me/${BuildConfig.FEEDBACK_TELEGRAM}"))
+            .setPackage("org.telegram.messenger")
     }
 }
