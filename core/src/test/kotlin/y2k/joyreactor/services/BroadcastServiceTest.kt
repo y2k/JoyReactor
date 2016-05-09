fcaccf19baf116f3c5b0766d690a09b8a0fd0196
y2k <itwisterlx@gmail.com>
2016-05-10 00:07:35 +0400

    tests

--- core/src/test/kotlin/y2k/joyreactor/services/BroadcastServiceTest.kt
+++ core/src/test/kotlin/y2k/joyreactor/services/BroadcastServiceTest.kt
@@ -3,9 +3,11 @@ package y2k.joyreactor.services
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import rx.Single
 import rx.schedulers.Schedulers
 import y2k.joyreactor.common.ForegroundScheduler
 import y2k.joyreactor.common.Notifications
+import y2k.joyreactor.common.subscribe
 
 /**
  * Created by y2k on 5/2/16.
@@ -32,5 +34,33 @@ class BroadcastServiceTest {
         BroadcastService.unregisterToken(Notifications.Posts)
         BroadcastService.broadcast(Notifications.Posts)
         assertEquals(1, callCount)
+    }
+
+    @Test
+    fun testLifeCycleService() {
+        val single = Single.fromCallable { "" } to Notifications.Posts
+        val lifeCycleService = LifeCycleService(BroadcastService)
+
+        var count = 0
+        single.subscribe(lifeCycleService) { count++ }
+
+        assertEquals(1, count)
+
+        BroadcastService.broadcast(Notifications.Posts)
+        assertEquals(1, count)
+
+        lifeCycleService.activate()
+        assertEquals(2, count)
+
+        BroadcastService.broadcast(Notifications.Posts)
+        assertEquals(3, count)
+        BroadcastService.broadcast(Notifications.Posts)
+        assertEquals(4, count)
+
+        lifeCycleService.deactivate()
+        assertEquals(4, count)
+
+        BroadcastService.broadcast(Notifications.Posts)
+        assertEquals(4, count)
     }
 }
