7247eaf47a63b1013b1893b5e1dd35a355dd335e
y2k <itwisterlx@gmail.com>
2016-05-28 21:24:40 +0400

    telegram username fix

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 : ${SIGN_KEYSTORE?"Need to set SIGN_KEYSTORE"}
 : ${SIGN_PASSWORD?"Need to set SIGN_PASSWORD"}
+: ${FEEDBACK_TELEGRAM?"Need to set FEEDBACK_TELEGRAM"}
 
 export SING_BUILD=$(git rev-list --count master)0
 
