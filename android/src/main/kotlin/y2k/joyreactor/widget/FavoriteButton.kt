67a4769521c0606b110ebd7a23f554f1e34cf317
y2k <itwisterlx@gmail.com>
2016-05-29 11:56:36 +0400

    Android UI for favorite

--- android/src/main/kotlin/y2k/joyreactor/widget/FavoriteButton.kt
+++ android/src/main/kotlin/y2k/joyreactor/widget/FavoriteButton.kt
@@ -5,19 +5,23 @@ import android.graphics.Color
 import android.support.v7.widget.AppCompatImageButton
 import android.util.AttributeSet
 import y2k.joyreactor.R
+import y2k.joyreactor.common.getColorCompat
 import y2k.joyreactor.common.property
 
 /**
  * Created by y2k on 5/29/16.
  */
-class FavoriteButton(context: Context?, attrs: AttributeSet?) : AppCompatImageButton(context, attrs) {
+class FavoriteButton(context: Context, attrs: AttributeSet?) : AppCompatImageButton(context, attrs) {
 
     val isFavorite = property(false)
 
+    private val selectedColor = context.getColorCompat(R.color.primary)
+    private val unselectedColor = Color.LTGRAY
+
     init {
-        setImageResource(R.drawable.ic_star)
+        setImageResource(R.drawable.ic_stars_white_24dp)
         isFavorite.subscribe {
-            setColorFilter(if (it) Color.YELLOW else Color.LTGRAY)
+            setColorFilter(if (it) selectedColor else unselectedColor)
         }
     }
 }
