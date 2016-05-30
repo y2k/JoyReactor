63fcc4c07b944ce34ce3a30481584540fb5cb656
y2k <itwisterlx@gmail.com>
2016-05-30 18:05:31 +0400

    add firebase notifications

---
+++ android/src/main/kotlin/y2k/joyreactor/UpdateService.kt
@@ -0,0 +1,12 @@
+package y2k.joyreactor
+
+import com.google.firebase.iid.FirebaseInstanceIdService
+import com.google.firebase.messaging.FirebaseMessagingService
+
+/**
+ * Created by y2k on 5/30/16.
+ */
+class UpdateService : FirebaseMessagingService() {
+
+    class InstanceService : FirebaseInstanceIdService()
+}
