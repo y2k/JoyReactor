d70e7b4f908df0122c216d0f4bf96b61456225e3
y2k <itwisterlx@gmail.com>
2016-06-19 10:24:57 +0400

    refactoring

--- core/src/main/kotlin/y2k/joyreactor/model/Image.kt
+++ core/src/main/kotlin/y2k/joyreactor/model/Image.kt
@@ -22,20 +22,13 @@ class Image(
         }
     }
 
-    fun thumbnailUrl(width: Int?, height: Int?): String {
-        try {
-            return toURL(width, height, null).toString()
-        } catch (e: MalformedURLException) {
-            throw RuntimeException(e)
-        }
-    }
+    fun thumbnailUrl(width: Int?, height: Int?): String = toURL(width, height, null).toString()
 
     private fun toURL(width: Int?, height: Int?, format: String?): URL {
+        val base = URL("https://azure.y2k.work/")
         if (width == null || height == null)
-            return URL("https", "azure.y2k.work", 443, "/cache/original?url=$url${getFormatPart(format)}")
-        return URL(
-            "https", "azure.y2k.work", 443,
-            "/cache/fit?quality=30&bgColor=ffffff&width=$width&height=$height&url=$url")
+            return URL(base, "/cache/original?url=$url${getFormatPart(format)}")
+        return URL(base, "/cache/fit?quality=30&bgColor=ffffff&width=$width&height=$height&url=$url")
     }
 
     private fun getFormatPart(format: String?): String {
