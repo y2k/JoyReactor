35bd83aa4b21bb90e695a479fc7d262adf58a23a
y2k <itwisterlx@gmail.com>
2016-07-04 17:19:13 +0400

    refactoring

--- core/src/main/kotlin/y2k/joyreactor/services/ImageService.kt
+++ core/src/main/kotlin/y2k/joyreactor/services/ImageService.kt
@@ -1,14 +1,13 @@
 package y2k.joyreactor.services
 
-import rx.Completable
 import rx.Observable
 import rx.Subscription
 import rx.subjects.BehaviorSubject
-import y2k.joyreactor.common.ForegroundScheduler
 import y2k.joyreactor.common.http.HttpClient
 import y2k.joyreactor.common.mapNotNull
 import y2k.joyreactor.common.platform.Platform
 import y2k.joyreactor.common.replaceIfNull
+import y2k.joyreactor.common.ui
 import y2k.joyreactor.model.Image
 import y2k.joyreactor.services.images.DiskCache
 import java.util.*
@@ -25,41 +24,45 @@ class ImageService(
         return image?.thumbnailUrl(width, height)
     }
 
-    fun <T> to(state: LinksPool, image: String?, target: Any): Observable<T> {
-        if (image == null) {
-            state.sLinks.remove(target)?.let { it.unsubscribe() }
-            return Observable.just(null)
-        }
+    fun <T> to(state: LinksPool, imageUrl: String?, target: Any): Observable<T> {
+        state.sLinks.remove(target)?.let { it.unsubscribe() }
+        if (imageUrl == null) return Observable.just(null)
 
         val publish = BehaviorSubject.create<T>()
         var subscription: Subscription? = null
-        subscription = getFromCache<T>(image)
-            .replaceIfNull { putToCache(image).andThen(getFromCache<T>(image)) }
-            .map { it!! }
-            .observeOn(ForegroundScheduler.instance)
-            .subscribe({
+        subscription = getFromCache<T>(imageUrl)
+            .replaceIfNull { putToCache(imageUrl) }
+            .ui({
                 if (state.sLinks[target] === subscription) {
                     publish.onNext(it)
                     state.sLinks.remove(target)
                 }
+
                 publish.onCompleted()
             }, {
                 it.printStackTrace()
+                if (state.sLinks[target] === subscription)
+                    state.sLinks.remove(target)
+                
                 publish.onCompleted()
             })
 
         publish.onNext(null)
-        state.sLinks.put(target, subscription)?.let { it.unsubscribe() }
+        state.sLinks.put(target, subscription)
         return publish
     }
 
-    private fun <T> getFromCache(url: String): Observable<T?> {
-        return diskCache.get(url).mapNotNull { decoder.decodeImage<T>(it) }.toObservable()
+    private fun <T> putToCache(url: String): Observable<T> {
+        val tmp = createTempFile(directory = diskCache.cacheDirectory)
+        return client.downloadToFile(url, tmp)
+            .andThen(diskCache.put(tmp, url))
+            .andThen(getFromCache<T>(url).map { it!! })
     }
 
-    private fun putToCache(url: String): Completable {
-        val tmp = createTempFile(directory = diskCache.cacheDirectory)
-        return client.downloadToFile(url, tmp).andThen(diskCache.put(tmp, url))
+    private fun <T> getFromCache(url: String): Observable<T?> {
+        return diskCache.get(url)
+            .mapNotNull { decoder.decodeImage<T>(it) }
+            .toObservable()
     }
 }
 
